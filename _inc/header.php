3006497d84afb6dd80e64d6799e316d6f8f57aa3
Sam Brannon <sam@branberg.com>
2014-10-17 13:34:32 -0500

    Revised OG description tag

--- _inc/header.php
+++ _inc/header.php
@@ -7,13 +7,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/png" href="/library/img/favicon.png" />
 
-	<meta name="description" content="<?php echo ( $ogDescription ? $ogDescription : 'A designer and developer doing what we love for people who love what they do.' ); ?>"/>
+	<meta name="description" content="<?php echo ( $ogDescription ? $ogDescription : 'We craft digital experiences - A studio committed to great ideas.' ); ?>"/>
 	<link rel="canonical" href="http://branberg.com/" />
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Branberg" />
 	<meta property="og:title" content="<?php echo $pageTitle; ?> - Branberg" />
-	<meta property="og:description" content="<?php echo ( $ogDescription ? $ogDescription : 'A designer and developer doing what we love for people who love what they do.' ); ?>" />
+	<meta property="og:description" content="<?php echo ( $ogDescription ? $ogDescription : 'We craft digital experiences - A studio committed to great ideas.' ); ?>" />
 	<meta property="og:url" content="http://branberg.com/" />
 	<meta property="og:image" content="<?php echo ( $ogImageURL ? $ogImageURL : 'http://branberg.com/library/img/facebook.png' ); ?>" />
 	<meta property="article:publisher" content="https://www.facebook.com/branbergstudios" />
