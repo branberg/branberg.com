636888b3d1c1e173e0a3151fb4d189e84978d150
Sam Brannon <sam@branberg.com>
2014-10-17 13:26:49 -0500

    Rearranged footer elements

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,11 +1,11 @@
 
     <footer id="main_footer">
       <ul class="clearfix">
-        <li class="copyright">&copy;Branberg <?php echo date('Y'); ?></li>
-        <li class="location">St. Paul, MN</li>
-        <li class="phone"><a href="tel:5073636786">1.612.524.5562</a></li>
         <li class="email"><a href="mailto:hello@branberg.com">hello@branberg.com</a></li>
-        <li class="twitter right_align"><a href="http://twitter.com/branberg" target="_blank">@branberg</a></li>
+        <li class="phone"><a href="tel:5073636786">1.612.524.5562</a></li>
+        <li class="twitter"><a href="http://twitter.com/branberg" target="_blank">@branberg</a></li>
+        <li class="location">St. Paul, MN</li>
+        <li class="copyright right_align">&copy;Branberg <?php echo date('Y'); ?></li>
       </ul>
     </footer>
 
