001fe4359ddb6781b7524e36a22042555819eb33
Sam Brannon <sam@branberg.com>
2014-10-21 19:42:35 -0500

    COnverted email to unicode to help ward off spammers

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,7 +1,7 @@
 
     <footer id="main_footer">
       <ul class="clearfix">
-        <li class="email"><a href="mailto:hello@branberg.com">hello@branberg.com</a></li>
+        <li class="email"><a href="mailto:&#104;&#101;&#108;&#108;&#111;&#64;&#98;&#114;&#97;&#110;&#98;&#101;&#114;&#103;&#46;&#99;&#111;&#109;">&#104;&#101;&#108;&#108;&#111;&#64;&#98;&#114;&#97;&#110;&#98;&#101;&#114;&#103;&#46;&#99;&#111;&#109;</a></li>
         <li class="phone"><a href="tel:5073636786">1.612.524.5562</a></li>
         <li class="twitter"><a href="http://twitter.com/branberg" target="_blank">@branberg</a></li>
         <li class="location">St. Paul, MN</li>
@@ -15,7 +15,7 @@
     <div class="md-content">
       <div class="main_contact">
         <p class="number">612.524.5562</p>
-        <p class="email"><a href="mailto:hello@branberg.com">hello@branberg.com</a></p>
+        <p class="email"><a href="mailto:&#104;&#101;&#108;&#108;&#111;&#64;&#98;&#114;&#97;&#110;&#98;&#101;&#114;&#103;&#46;&#99;&#111;&#109;">&#104;&#101;&#108;&#108;&#111;&#64;&#98;&#114;&#97;&#110;&#98;&#101;&#114;&#103;&#46;&#99;&#111;&#109;</a></p>
       </div>
       <div class="secondary_contact">
         <p class="address">St. Paul, MN</p>
