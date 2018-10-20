7b551bd6fe313b7aabec06eef4ee67fb1bb81aa3
Sam Brannon <sam@reverb.com>
2018-10-20 17:21:15 -0500

    Added README and refactored modal js to include esc keypresses

---
+++ README.md
@@ -0,0 +1,22 @@
+# Branberg.com
+
+The website for Branberg
+
+## Running Locally
+
+```bash
+$ bundle install
+$ bundle exec jekyll serve
+```
+
+That will spin up the local dev server on [http://localhost:4000](localhost:4000)
+
+## Building
+
+This site is hosted by [Netlify](https://netlify.com) and automatically builds anything on `master`.
+
+To build locally:
+
+```bash
+$ bundle exec jekyll build
+```
--- assets/js/modal/modalEffects.js
+++ assets/js/modal/modalEffects.js
@@ -10,40 +10,33 @@
  */
 var ModalEffects = (function() {
 
+  function closeModal(modal, triggerElem) {
+    classie.remove(modal, 'md-show');
+  }
+
   function init() {
+    var overlay = document.querySelector('.md-overlay');
 
-    var overlay = document.querySelector( '.md-overlay' );
-
-    [].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
-
+    [].slice.call(document.querySelectorAll('.md-trigger')).forEach( function( el, i ) {
       var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) )
 
-      function removeModal( hasPerspective ) {
-        classie.remove( modal, 'md-show' );
-
-        if( hasPerspective ) {
-          classie.remove( document.documentElement, 'md-perspective' );
-        }
-      }
-
-      function removeModalHandler() {
-        removeModal( classie.has( el, 'md-setperspective' ) );
-      }
-
-      el.addEventListener( 'click', function( ev ) {
+      el.addEventListener( 'click', function() {
         classie.add( modal, 'md-show' );
-        overlay.removeEventListener( 'click', removeModalHandler );
-        overlay.addEventListener( 'click', removeModalHandler );
-
-        if( classie.has( el, 'md-setperspective' ) ) {
-          setTimeout( function() {
-            classie.add( document.documentElement, 'md-perspective' );
-          }, 25 );
-        }
       });
 
-    } );
+      overlay.addEventListener('click', function() {
+        closeModal(modal, el);
+      });
 
+      document.onkeydown = function(event) {
+        event = event || window.event;
+
+        if (event.keyCode == 27) {
+          closeModal(modal, el);
+        }
+      }
+
+    });
   }
 
   init();
